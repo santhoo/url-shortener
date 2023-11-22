932a31a687150b675df0c0b75cb16b96ba8e1629
Santiago Sipoli <santiago.wd@gmail.com>
2023-11-22 12:11:40 -0300

    Handle domain URL with method

--- src/util/general.js
+++ src/util/general.js
@@ -1,6 +1,6 @@
 const Domain = {
 	host: process.env.VERCEL_URL || process.env.APP_HOSTNAME,
-	url: process.env.APP_URL,
+	url: process.env.VERCEL_URL ? `https://${host}` : `http://${host}`,
 }
 
 export default Domain
