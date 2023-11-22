b525d47d16560bd7d71812e666002ac376a5001d
Santiago Sipoli <santiago.wd@gmail.com>
2023-11-22 10:53:29 -0300

    Fix process declaration

--- src/util/firebase/index.js
+++ src/util/firebase/index.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: proccess.env.FB_API_KEY,
-	authDomain: proccess.env.FB_AUTH_DOMAIN,
-	projectId: proccess.env.FB_PROJECT_ID,
-	storageBucket: proccess.env.FB_STORAGE_BUCKET,
-	messagingSenderId: proccess.env.FB_MESSAGING_SENDER_ID,
-	appId: proccess.env.FB_APP_ID,
+	apiKey: process.env.FB_API_KEY,
+	authDomain: process.env.FB_AUTH_DOMAIN,
+	projectId: process.env.FB_PROJECT_ID,
+	storageBucket: process.env.FB_STORAGE_BUCKET,
+	messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+	appId: process.env.FB_APP_ID,
 }
 
 // Initialize Firebase
